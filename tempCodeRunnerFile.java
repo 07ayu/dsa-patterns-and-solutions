4c17e0ed18b7e6f3e0771f19995056f8203dfa16
Ayushman <ayushmantripathi283@gmail.com>
2026-04-20 17:28:21 +0530

    leetcode 80

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,3 +1,35 @@
 public class tempCodeRunnerFile {
+
+    public static int removeDuplicates(int[] nums) {
+        int sp = 0;
+        int fp = 1;
+
+        while (fp < nums.length) {
+
+            if (nums[sp] != nums[fp]) {
+                System.out.println(sp);
+                nums[sp + 1] = nums[fp];
+                sp++;
+            }
+            
+
+            fp++;
+        }
+        // System.out.println(cp);
+        return sp+1;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        
+        int[] arr = { 0, 0, 1, 1, 1, 1, 2, 3, };
+
+        int k = removeDuplicates(arr);
+
+        for (int i : arr) {
+            System.out.print(arr[i] + " ");
+        }
+    }
     
 }
